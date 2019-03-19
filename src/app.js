e54b6d2ad91f4b88145c26cf75202c6810048ea6
Teresa Van <teresa.van@ucalgary.ca>
2019-03-18 18:22:22 -0600

    ABout to push data to GITHUB

--- src/app.js
+++ src/app.js
@@ -1,15 +1,4 @@
-import {Deck, Layer} from '@deck.gl/core';
-import
-{
-	GeoJsonLayer,
-	PathLayer,
-	ScatterplotLayer
-} from '@deck.gl/layers';
-import mapboxgl from 'mapbox-gl';
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import DrawRectangle from 'mapbox-gl-draw-rectangle-mode';
-import MapboxDraw from 'mapbox-gl-draw'; 
-import * as data from './data';
+const {MapboxLayer, PathLayer} = deck;
 
 const INITIAL_VIEW_STATE =
 {
@@ -35,109 +24,14 @@ var view = INITIAL_VIEW_STATE;
 // Create base map
 const map = new mapboxgl.Map
 ({
-	container: 'map',
+	container: document.body,
 	style: 'http://162.246.156.156:8080/styles/dark-matter/style.json', // Using our own map server as opposed to Mapbox's server
 	// Note: deck.gl will be in charge of interaction and event handling
-	interactive: false,
+	interactive: true,
 	center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
 	zoom: INITIAL_VIEW_STATE.zoom,
 	bearing: INITIAL_VIEW_STATE.bearing,
 	pitch: INITIAL_VIEW_STATE.pitch,
-});
-
-const selectMap = new mapboxgl.Map
-({
-	container: 'select-map',
-	style: 'http://162.246.156.156:8080/styles/dark-matter/style.json', // Using our own map server as opposed to Mapbox's server
-	interactive: false,
-	center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
-	zoom: INITIAL_VIEW_STATE.zoom,
-	bearing: INITIAL_VIEW_STATE.bearing,
-	pitch: INITIAL_VIEW_STATE.pitch,
-});
-
-selectMap.on('load', e =>
-{
-	selectMap.getStyle().layers.forEach(l => 
-	{
-		if (!l.id.startsWith('gl-draw')) 
-			selectMap.setLayoutProperty(l.id, 'visibility', 'none')
-	});
-});
-
-var draw = new MapboxDraw
-({
-	displayControlsDefault: false,
-	controls: 
-	{
-		polygon: true,
-		trash: true
-	}
-});
-
-selectMap.addControl(draw);
-selectMap.on('draw.create', updateArea);
-selectMap.on('draw.delete', updateArea);
-selectMap.on('draw.update', updateArea);
- 
-function updateArea(e) 
-{
-	var data = draw.getAll();
-	console.log(data);
-	// var answer = document.getElementById('calculated-area');
-	// if (data.features.length > 0) {
-	// var area = turf.area(data);
-	// // restrict to area to 2 decimal points
-	// var rounded_area = Math.round(area*100)/100;
-	// answer.innerHTML = '<p><strong>' + rounded_area + '</strong></p><p>square meters</p>';
-	// } else {
-	// answer.innerHTML = '';
-	// if (e.type !== 'draw.delete') alert("Use the draw tools to draw a polygon!");
-	// }
-}
-
-// Create buildings from campus shape files
-const buildings = new GeoJsonLayer
-({
-	id: 'geojsonLayer',
-	data: data.GEOJSON,
-	stroked: true,
-	filled: true,
-	getLineColor: [100, 100, 100],
-	getFillColor: [150, 150, 150, 100],
-	opacity: 0.25,
-	lineWidthMinPixels: 2,
-	pickable: true,
-	fp64: true,
-	lightSettings: LIGHT_SETTINGS,
-	onClick: ({object, x, y}) =>
-	{
-		try
-		{
-			const tooltip = object.properties.Building_n;
-			console.log(tooltip);
-		}
-		catch(error) { };
-		/* Update tooltip
-			http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
-		*/
-	}
-});
-
-const staypoints = new ScatterplotLayer
-({
-	id: 'scatterplotLayer',
-	data: data.STAYPOINTSVISUAL,
-    getPosition: p => p.point,
-    getFillColor: [255, 250, 0],
-    opacity: 0.01,
-    getRadius: 0.5,
-    radiusScale: 2 ** (30 - view.zoom),
-    radiusMinPixels: 0.1,
-    radiusMaxPixels: 2,
-	pickable: false,
-    lightSettings: LIGHT_SETTINGS
-	
 });
 
 // Create paths
@@ -157,36 +51,177 @@ const paths = new PathLayer
 	lightSettings: LIGHT_SETTINGS
 });
 
-// Create and render deck
-export const deck = new Deck
-({
-	canvas: 'deck-canvas',
-	width: '100%',
-	height: '100%',
-	initialViewState: INITIAL_VIEW_STATE,
-	controller: true,
-	onViewStateChange: ({viewState}) => 
-	{
-		map.jumpTo
-		({
-			center: [viewState.longitude, viewState.latitude],
-			zoom: viewState.zoom,
-			bearing: viewState.bearing,
-			pitch: viewState.pitch
-		});
-		selectMap.jumpTo
-		({
-			center: [viewState.longitude, viewState.latitude],
-			zoom: viewState.zoom,
-			bearing: viewState.bearing,
-			pitch: viewState.pitch
-		});
-		view = viewState;
-	},
-	layers:
-	[
-		buildings,
-		paths,
-		// staypoints,
-	]
+map.on('load', () => 
+{
+	map.addLayer(paths);
 });
+// var draw = new MapboxDraw
+// ({
+// 	displayControlsDefault: false,
+// 	controls:
+// 	{
+// 		polygon: true,
+// 		trash: true
+// 	}
+// });
+
+// map.addControl(draw);
+// map.on('draw.create', updateArea);
+// map.on('draw.delete', updateArea);
+// map.on('draw.update', updateArea);
+
+// // selectMap.disable();
+
+// function updateArea(e) 
+// {
+// 	var data = draw.getAll();
+// 	console.log(data);
+// }
+
+// // Create paths
+// const paths = new MapboxLayer
+// ({
+// 	id: 'pathLayer',
+// 	type: PathLayer,
+//     data: [],
+//     // getPath: p => p.path,
+//     // getColor: p => p.azimuthColor,
+//     // opacity: 0.01 * (data.maxPaths / data.PATHSVISUAL.length / 5),
+// 	// getWidth: 2,
+// 	// widthScale: 2 ** (15 - view.zoom),
+// 	// widthMinPixels: 0.1,
+// 	// widthMaxPixels: 2,
+// 	// rounded: true,
+//     // pickable: false,
+// 	// lightSettings: LIGHT_SETTINGS
+// });
+
+// // Insert the layer before mapbox labels 
+// map.on('load', () => 
+// {
+// 	map.addLayer(paths, 'waterway-label');
+// });
+
+// const myScatterplotLayer = new MapboxLayer
+// ({
+//     id: 'my-scatterplot',
+// 	type: ScatterplotLayer,
+//     data: [
+//         {position: [-74.5, 40], size: 100}
+//     ],
+//     getPosition: d => d.position,
+//     getRadius: d => d.size,
+//     getColor: [255, 0, 0]
+// });
+
+// myScatterplotLayer.map = map;
+
+// console.log(myScatterplotLayer);
+
+// map.on('load', () => 
+// {
+// 	map.addLayer(myScatterplotLayer);
+// });
+
+// add to mapbox
+// map.addLayer(myScat	terplotLayer);
+
+// // update the layer
+// myScatterplotLayer.setProps({
+//     getColor: [0, 0, 255]
+// });
+
+// Create buildings from campus shape files
+// const buildings = new GeoJsonLayer
+// ({
+// 	id: 'geojsonLayer',
+// 	data: data.GEOJSON,
+// 	stroked: true,
+// 	filled: true,
+// 	getLineColor: [100, 100, 100],
+// 	getFillColor: [150, 150, 150, 100],
+// 	opacity: 0.25,
+// 	lineWidthMinPixels: 2,
+// 	pickable: true,
+// 	fp64: true,
+// 	lightSettings: LIGHT_SETTINGS,
+// 	onClick: ({object, x, y}) =>
+// 	{
+// 		try
+// 		{
+// 			const tooltip = object.properties.Building_n;
+// 			console.log(tooltip);
+// 		}
+// 		catch(error) { };
+// 		/* Update tooltip
+// 			http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
+// 		*/
+// 	}
+// });
+
+// const staypoints = new ScatterplotLayer
+// ({
+// 	id: 'scatterplotLayer',
+// 	data: data.STAYPOINTSVISUAL,
+//     getPosition: p => p.point,
+//     getFillColor: [255, 250, 0],
+//     opacity: 0.01,
+//     getRadius: 0.5,
+//     radiusScale: 2 ** (30 - view.zoom),
+//     radiusMinPixels: 0.1,
+//     radiusMaxPixels: 2,
+// 	pickable: false,
+//     lightSettings: LIGHT_SETTINGS
+
+// });
+
+// // Create paths
+// const paths = new PathLayer
+// ({
+//     id: 'pathLayer',
+//     data: data.PATHSVISUAL,
+//     getPath: p => p.path,
+//     getColor: p => p.azimuthColor,
+//     opacity: 0.01 * Math.sqrt(data.maxPaths / data.PATHSVISUAL.length, 2),
+// 	getWidth: 2,
+// 	widthScale: 2 ** (15 - view.zoom),
+// 	widthMinPixels: 0.1,
+// 	widthMaxPixels: 2,
+// 	rounded: true,
+//     pickable: false,
+// 	lightSettings: LIGHT_SETTINGS
+// });
+
+// // Create and render deck
+// export const deck = new Deck
+// ({
+// 	canvas: 'deck-canvas',
+// 	width: '100%',
+// 	height: '100%',
+// 	initialViewState: INITIAL_VIEW_STATE,
+// 	controller: true,
+// 	onViewStateChange: ({viewState}) => 
+// 	{
+// 		map.jumpTo
+// 		({
+// 			center: [viewState.longitude, viewState.latitude],
+// 			zoom: viewState.zoom,
+// 			bearing: viewState.bearing,
+// 			pitch: viewState.pitch
+// 		});
+// 		// selectMap.jumpTo
+// 		// ({
+// 		// 	center: [viewState.longitude, viewState.latitude],
+// 		// 	zoom: viewState.zoom,
+// 		// 	bearing: viewState.bearing,
+// 		// 	pitch: viewState.pitch
+// 		// });
+// 		view = viewState;
+// 	},
+// 	// layers:
+// 	// [
+// 	// 	buildings,
+// 	// 	paths,
+// 	// 	// staypoints,
+// 	// ]
+// });
--- src/filter.js
+++ src/filter.js
@@ -1,88 +1,94 @@
-/*
-    Filters: 
-        [lowerBound, upperBound] <- Select all values that range between lower and upper bound
-        value <- Select value equal to value
-        function(d) { return d } <- Select values equal to result of the function
-        null <- Select all (remove filter)
-*/
+// /*
+//     Filters: 
+//         [lowerBound, upperBound] <- Select all values that range between lower and upper bound
+//         value <- Select value equal to value
+//         function(d) { return d } <- Select values equal to result of the function
+//         null <- Select all (remove filter)
+// */
 
-import * as crossfilter from 'crossfilter2';
-import {default as pathjson} from '../data/paths.json';
-import {default as staypointsjson} from '../data/staypoints.json';
+// // import * as crossfilter from 'crossfilter2';
+// // import {default as pathjson} from '../data/paths.json';
+// // import {default as staypointsjson} from '../data/staypoints.json';
 
-export const paths = crossfilter(pathjson);
-export const maxPaths = paths.groupAll().reduceCount().value();
-export const staypoints = crossfilter(staypointsjson);
-configurePathsFilter();
-// filterPathAcademicDay('BETWEEN_TERMS');
-// filterLatitude([51.077820, 51.078580]);
-// filterLongitude([-114.13050, -114.12840]);
+// var pathsURL = '../data/paths.json';
+// var staypointsURL = '../data/staypoints.json';
 
-// #region Paths
+// var pathjson = d3.json(pathsURL);
+// var staypointjson = d3.json(staypointsURL);
 
-function filterPathID(id)
-{
-    paths.id.filter(id);
-}
+// const paths = crossfilter(pathjson);
+// const maxPaths = paths.groupAll().reduceCount().value();
+// const staypoints = crossfilter(staypointsjson);
+// configurePathsFilter();
+// filterPathAcademicDay('FALL_TERM_1');
+// // filterLatitude([51.077820, 51.078580]);
+// // filterLongitude([-114.13050, -114.12840]);
 
-function filterPathAcademicDay(academicDay)
-{
-    paths.academicDay.filter(academicDay);
-}
+// // #region Paths
 
-function filterPathBuildingID(buildingId)
-{
-    paths.buildingId.filter(buildingId);
-}
+// function filterPathID(id)
+// {
+//     paths.id.filter(id);
+// }
 
-function filterPathBuildingName(buildingName)
-{
-    paths.buildingName.filter(buildingName);
-}
+// function filterPathAcademicDay(academicDay)
+// {
+//     paths.academicDay.filter(academicDay);
+// }
 
-function filterPathMaxTemp(maxTemp)
-{
-    paths.maxTemp.filter(maxTemp);
-}
+// function filterPathBuildingID(buildingId)
+// {
+//     paths.buildingId.filter(buildingId);
+// }
 
-function filterPathMeanTemp(meanTemp)
-{
-    paths.meanTemp.filter(meanTemp);
-}
+// function filterPathBuildingName(buildingName)
+// {
+//     paths.buildingName.filter(buildingName);
+// }
 
-function filterLatitude(latitude)
-{
-    paths.latitude.filter(latitude);
-}
+// function filterPathMaxTemp(maxTemp)
+// {
+//     paths.maxTemp.filter(maxTemp);
+// }
 
-function filterLongitude(longitude)
-{
-    paths.longitude.filter(longitude);
-}
+// function filterPathMeanTemp(meanTemp)
+// {
+//     paths.meanTemp.filter(meanTemp);
+// }
 
-function configurePathsFilter()
-{
-    try 
-    {
-        paths.id = paths.dimension(function(d) { return d.Path_ID}),
-        paths.pointId = paths.dimension(function(d) { return d.Path_Point_ID}),
-        paths.date = paths.dimension(function(d) { return d.Loct}), // Needs to parse properly
-        paths.academicDay = paths.dimension(function(d) { return d.Academic_Day}),
-        paths.buildingId = paths.dimension(function(d) { return d.Building_ID}),
-        paths.buildingName = paths.dimension(function(d) { return d.Building_Name}),
-        paths.latitude = paths.dimension(function(d) { return d.Lat}),
-        paths.longitude = paths.dimension(function(d) { return d.Lon}),
-        paths.distanceToNext = paths.dimension(function(d) { return d.Distance_To_Next}),
-        paths.minutesToNext = paths.dimension(function(d) { return d.Minutes_To_Next}),
-        paths.maxTemp = paths.dimension(function(d) { return d.Max_Temp_C}),
-        paths.meanTemp = paths.dimension(function(d) { return d.Mean_Temp_C}),
-        paths.totalPrecip = paths.dimension(function(d) { return d.Total_Precip_mm}),
-        paths.snow = paths.dimension(function(d) { return d.Snow_cm}),
-        paths.azimuthPath = paths.dimension(function(d) { return d.Azimuth_Path}),
-        paths.azimuthSegment = paths.dimension(function(d) { return d.Azimuth_Segment}),
-        paths.speed = paths.dimension(function(d) { return d.Speed});
-    } 
-    catch (e) { console.log(e.stack); }
-}
+// function filterLatitude(latitude)
+// {
+//     paths.latitude.filter(latitude);
+// }
 
-// #endregion
+// function filterLongitude(longitude)
+// {
+//     paths.longitude.filter(longitude);
+// }
+
+// function configurePathsFilter()
+// {
+//     try 
+//     {
+//         paths.id = paths.dimension(function(d) { return d.Path_ID}),
+//         paths.pointId = paths.dimension(function(d) { return d.Path_Point_ID}),
+//         paths.date = paths.dimension(function(d) { return d.Loct}), // Needs to parse properly
+//         paths.academicDay = paths.dimension(function(d) { return d.Academic_Day}),
+//         paths.buildingId = paths.dimension(function(d) { return d.Building_ID}),
+//         paths.buildingName = paths.dimension(function(d) { return d.Building_Name}),
+//         paths.latitude = paths.dimension(function(d) { return d.Lat}),
+//         paths.longitude = paths.dimension(function(d) { return d.Lon}),
+//         paths.distanceToNext = paths.dimension(function(d) { return d.Distance_To_Next}),
+//         paths.minutesToNext = paths.dimension(function(d) { return d.Minutes_To_Next}),
+//         paths.maxTemp = paths.dimension(function(d) { return d.Max_Temp_C}),
+//         paths.meanTemp = paths.dimension(function(d) { return d.Mean_Temp_C}),
+//         paths.totalPrecip = paths.dimension(function(d) { return d.Total_Precip_mm}),
+//         paths.snow = paths.dimension(function(d) { return d.Snow_cm}),
+//         paths.azimuthPath = paths.dimension(function(d) { return d.Azimuth_Path}),
+//         paths.azimuthSegment = paths.dimension(function(d) { return d.Azimuth_Segment}),
+//         paths.speed = paths.dimension(function(d) { return d.Speed});
+//     } 
+//     catch (e) { console.log(e.stack); }
+// }
+
+// // #endregion
